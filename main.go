390cae22651af0369dc2db73192183f5e67db38b
dora <31735614+dora1998@users.noreply.github.com>
2019-10-10 18:43:41 +0900

    Create http server

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type CallbackBody struct {
+	Text        string `json:"text"`
+	UserName    string `json:"user_name"`
+	LinkToTweet string `json:"link_to_tweet"`
+	CreatedAt   string `json:"created_at"`
+}
+
+func main() {
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		body := &CallbackBody{}
+		err := json.NewDecoder(r.Body).Decode(body)
+		if err != nil {
+			fmt.Printf(err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		fmt.Printf("%#v\n", body)
+	})
+
+	// ②
+	http.ListenAndServe(":8080", nil)
+}
