2f0c43bbe90561dd00e715852580deb531bd259f
dora <31735614+dora1998@users.noreply.github.com>
2019-10-16 15:21:21 +0900

    Implement command_handler

---
+++ command_handler.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type Command struct {
+	name       string
+	handleFunc func(body string, statusId int64)
+}
+
+type CommandHandler struct {
+	commands []*Command
+}
+
+func NewCommandHandler() *CommandHandler {
+	return &CommandHandler{commands: []*Command{}}
+}
+
+func (h *CommandHandler) resolve(text string, statusId int64) error {
+	regexpObj := regexp.MustCompile("^(\\S+)(\\s(.+))*$")
+	res := regexpObj.FindStringSubmatch(text)
+	if res == nil {
+		return fmt.Errorf("failed resolve (incorrect pattern)")
+	}
+
+	commandName, commandBody := res[1], res[3]
+	fmt.Printf("%s: %s\n", commandName, commandBody)
+	for _, c := range h.commands {
+		if commandName == c.name {
+			c.handleFunc(commandBody, statusId)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("failed resolve (no match)")
+}
+
+func (h *CommandHandler) addCommand(c *Command) {
+	h.commands = append(h.commands, c)
+}
--- main.go
+++ main.go
@@ -14,26 +14,63 @@ type CallbackBody struct {
 }
 
 func main() {
+	repo, _ := NewTaskRepository()
+
+	handler := NewCommandHandler()
+	handler.addCommand(&Command{
+		name: "追加",
+		handleFunc: func(body string, statusId int64) {
+			fmt.Printf("add: %s (%v)\n", body, statusId)
+			// TODO: 日付処理
+			repo.Add(body, "10/16")
+			client := NewTwitterClient()
+			client.reply(body, statusId)
+		},
+	})
+	handler.addCommand(&Command{
+		name: "一覧",
+		handleFunc: func(_ string, statusId int64) {
+			fmt.Printf("list (%v)\n", statusId)
+
+			output := ""
+			for _, t := range repo.GetAllTasks() {
+				output += t.Body + "\n"
+			}
+
+			client := NewTwitterClient()
+			client.reply(output, statusId)
+		},
+	})
+
 	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
-		body := &CallbackBody{}
-		err := json.NewDecoder(r.Body).Decode(body)
+		callbackBody := &CallbackBody{}
+		err := json.NewDecoder(r.Body).Decode(callbackBody)
 		if err != nil {
 			fmt.Printf(err.Error())
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-		fmt.Printf("%#v\n", body)
-		statusId, err := extractStatusIdFromUrl(body.LinkToTweet)
+		fmt.Printf("%#v\n", callbackBody)
+		statusId, err := extractStatusIdFromUrl(callbackBody.LinkToTweet)
 		if err != nil {
 			fmt.Printf(err.Error())
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-		client := NewTwitterClient()
-		client.reply(body.Text, statusId)
+		text, err := extractBody(callbackBody.Text)
+		if err != nil {
+			fmt.Printf(err.Error())
+			return
+		}
+
+		err = handler.resolve(text, statusId)
+		if err != nil {
+			fmt.Printf(err.Error())
+			return
+		}
 	})
 
 	_ = http.ListenAndServe(":8080", nil)
