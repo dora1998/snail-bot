3cc3466be1f1e5486f16db678c90103c8aa1c266
dora <31735614+dora1998@users.noreply.github.com>
2019-10-19 18:02:32 +0900

    Update models

--- models.go
+++ models.go
@@ -3,9 +3,9 @@ package main
 import "time"
 
 type Task struct {
-	Id        string
-	Body      string
-	Deadline  time.Time
-	CreatedAt time.Time
-	CreatedBy string
+	Id        string    `db:"id"`
+	Body      string    `db:"body"`
+	Deadline  time.Time `db:"deadline"`
+	CreatedAt time.Time `db:"created_at"`
+	CreatedBy string    `db:"created_by"`
 }
