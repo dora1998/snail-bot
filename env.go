384d7a19b99e598ee0ee62aaa3b8dc7ec4269d76
dora <31735614+dora1998@users.noreply.github.com>
2019-10-19 18:02:24 +0900

    Add db env

--- env.go
+++ env.go
@@ -1,8 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+)
+
 type Env struct {
 	ConsumerKey    string
 	ConsumerSecret string
 	Token          string
 	TokenSecret    string
+}
+
+type DatabaseConfig struct {
+	Username string `envconfig:"DB_USERNAME" required:"true"`
+	Password string `envconfig:"DB_PASSWORD" required:"true"`
+	Host     string `envconfig:"DB_HOST" required:"true"`
+	Port     string `envconfig:"DB_PORT" required:"true"`
+	Name     string `envconfig:"DB_NAME" required:"true"`
+}
+
+func (d *DatabaseConfig) GetDataSourceName() string {
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		d.Username,
+		d.Password,
+		d.Host,
+		d.Port,
+		d.Name,
+	)
+}
+
+func ReadDBConfig() (*DatabaseConfig, error) {
+	var config DatabaseConfig
+	if err := envconfig.Process("bot", &config); err != nil {
+		return nil, fmt.Errorf("failed to read dsn from env")
+	}
+	return &config, nil
 }
--- go.mod
+++ go.mod
@@ -5,8 +5,11 @@ go 1.13
 require (
 	github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f
 	github.com/dghubble/oauth1 v0.6.0
+	github.com/go-sql-driver/mysql v1.4.0
 	github.com/google/uuid v1.1.1
+	github.com/jmoiron/sqlx v1.2.0
 	github.com/kelseyhightower/envconfig v1.4.0
+	google.golang.org/appengine v1.6.5 // indirect
 )
 
 replace github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f => github.com/dora1998/go-twitter v0.0.0-20191014181053-ad41280878c1
