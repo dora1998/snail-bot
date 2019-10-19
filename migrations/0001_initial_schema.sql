5c139130164662bb8a73d38cf1f9a1d0b6ab8d44
dora <31735614+dora1998@users.noreply.github.com>
2019-10-19 18:39:41 +0900

    Create migration file

---
+++ migrations/0001_initial_schema.sql
@@ -0,0 +1,11 @@
+-- +migrate Up
+CREATE TABLE task (
+                      id varchar(36) NOT NULL PRIMARY KEY,
+                      body text NOT NULL,
+                      deadline datetime NOT NULL,
+                      created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                      created_by text
+);
+
+-- +migrate Down
+DROP TABLE task;
