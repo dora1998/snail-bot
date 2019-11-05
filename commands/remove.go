0daa0492cb30192115c0b8df867ba4841fd0a0ff
dora <31735614+dora1998@users.noreply.github.com>
2019-11-06 01:27:21 +0900

    Implement remove command

---
+++ commands/remove.go
@@ -0,0 +1,47 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/dora1998/snail-bot/repository"
+	"github.com/dora1998/snail-bot/utils"
+	"github.com/jmoiron/sqlx"
+)
+
+var cmdRemove = &Command{
+	Name: "削除",
+	HandleFunc: func(body string, username string, statusId int64, db *sqlx.DB) {
+		fmt.Printf("remove: %s (%v)\n", body, statusId)
+		client := utils.NewTwitterClient()
+
+		if !client.IsFollwing(username) {
+			fmt.Printf("PermissionError: not following @%v\n", username)
+			client.Reply("この操作はフォローされている人しかできません🙇‍♂️", statusId)
+			return
+		}
+
+		repo := repository.NewDBRepository(db)
+
+		task := repo.GetTaskByBody(body)
+		if task == nil {
+			fmt.Printf("TaskNotFound: %v\n", body)
+			client.Reply("該当するタスクが見つかりません", statusId)
+			return
+		}
+
+		err := repo.Remove(task.Id)
+		if err != nil {
+			fmt.Printf("DatabaseError: %#v\n", err)
+			return
+		}
+		fmt.Printf("removed: %#v\n", task)
+
+		err = client.CreateFavorite(statusId)
+		if err != nil {
+			fmt.Printf("FavoriteError: %#v\n", err)
+		}
+	},
+}
+
+func init() {
+	CmdHandler.AddCommand(cmdRemove)
+}
